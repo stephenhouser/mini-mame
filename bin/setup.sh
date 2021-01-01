84f1524656b821ba201ed1b59c852de16397682a
Stephen Houser <stephenhouser@gmail.com>
2021-01-01 11:16:44 -0500

    add screenrc

--- bin/setup.sh
+++ bin/setup.sh
@@ -48,9 +48,49 @@ pacman --noconfirm -S wine winetricks
 # mame				-- the emulator
 pacman --noconfirm -S mame
 
+# Install attract mode from AUR
+
+# xorg-fonts-misc	-- we need some basic fonts for X11
+pacman --noconform -S xorg-fonts-misc
+
+git clone https://aur.archlinux.org/attract.git
+cd attract
+makepkg -si
+cd ..
+
+git clone https://aur.archlinux.org/mame-git.git mame
+cd mame
+makepkg -si
+cd ..
+
+
+
 # Was experimenting with CDEmu. Don't seem to need it (yet)
 # vhba-module 	-- emulates SCSI devices (for CDEmu)
 # cdemu-clinet	-- emulates real CD drives with ISO images
 #pacman --noconform -S cdemu-client vhba-module
 #systemctl enable cdemu-daemon.service
 
+cat >> ~/.zshrc << EOF
+export EDITOR='vim'
+export PAGER=`which less`
+export LESS="-eFRX -x4"
+export MORE="-eFRX -x4"
+export MANPAGER="$PAGER -isX"
+
+alias vi=vim
+alias ls='ls --color=auto'
+
+if [ -d "$HOME/bin" ] ; then
+        PATH="$HOME/bin:$PATH"
+fi
+EOF
+
+# Enable automatic login to the console
+# https://wiki.archlinux.org/index.php/Getty
+mkdir -p /etc/systemd/system/getty@tty1.service.d
+cat >> /etc/systemd/system/getty@tty1.service.d/override.conf << EOF
+[Service]
+ExecStart=
+ExecStart=-/usr/bin/agetty --autologin mame --noclear %I $TERM
+EOF
