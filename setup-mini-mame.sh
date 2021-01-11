6a76d8bb131492282242171a518cc3228709b15a
Stephen Houser <stephenhouser@gmail.com>
2021-01-11 15:50:20 -0500

    adding daphne

--- setup-mini-mame.sh
+++ setup-mini-mame.sh
@@ -217,10 +217,10 @@ alias vi=vim
 #alias ls='ls --color=auto'
 
 if [ -d "\${HOME}/bin" ] ; then
-        PATH="\$HOME}/bin:$PATH"
+        PATH="\${HOME}/bin:$PATH"
 fi
 
-if [ -z "$DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ]; then
+if [ -z "\${DISPLAY}" ] && [ -n "\${XDG_VTNR}" ] && [ "\${XDG_VTNR}" -eq 1 ]; then
 	exec startx
 fi
 EOF
