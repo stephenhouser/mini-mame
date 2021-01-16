79032ff0d177d20b5211226368575dcb52a47c59
Stephen Houser <stephenhouser@gmail.com>
2021-01-16 16:55:50 -0500

    add backup

--- tools/save-content.sh
+++ tools/save-content.sh
@@ -4,19 +4,19 @@ function sync() {
 	sudo rsync -v --progress --modify-window=1 --update --recursive --no-links --times "$1" "$2"
 }
 
-sync daphne1.0 /mnt/daphne/
-sync shared/daphne /mnt/daphne/shared/
+sync ~/daphne1.0 /mnt/daphne/
+sync ~/shared/daphne /mnt/daphne/shared/
 
-sync scummvm2.2 /mnt/scummvm/
-sync shared/scummvm /mnt/scummvm/shared/
+sync ~/scummvm2.2 /mnt/scummvm/
+sync ~/shared/scummvm /mnt/scummvm/shared/
 
-sync fbneo /mnt/fbneo/
-sync shared/fbneo /mnt/fbneo/shared/
+sync ~/fbneo /mnt/fbneo/
+sync ~/shared/fbneo /mnt/fbneo/shared/
 
-sync mame0.37b5	/mnt/mame/
-sync mame0.78+ /mnt/mame/
-sync mame0.227 /mnt/mame/
-sync shared/mame /mnt/mame/shared/
+sync ~/mame0.37b5	/mnt/mame/
+sync ~/mame0.78+ /mnt/mame/
+sync ~/mame0.227 /mnt/mame/
+sync ~/shared/mame /mnt/mame/shared/
 
-sync .attract/ /mnt/frontend/attract
-sync .config/retroarch /mnt/frontend/
+sync ~/.attract/ /mnt/frontend/attract
+sync ~/.config/retroarch /mnt/frontend/
